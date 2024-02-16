71ee0ae6399014cf94b38cf494510aeb1dfc9dad
Prashant <42613882+PrashantSakre@users.noreply.github.com>
2024-02-16 10:28:35 +0530

    Basic Elysia setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(swagger());
+
+app.get("/", () => "Hello Elysia").listen(process.env.PORT || 3000);
+
+app.get("/api/id/:id", ({ params: { id } }) => id);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
