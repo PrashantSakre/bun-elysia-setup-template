7f84bfdaaa9239a5dcbc65dddd25dc872679b2bb
Prashant <42613882+PrashantSakre@users.noreply.github.com>
2024-02-17 13:21:33 +0530

    add type checking in request

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,10 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { findAllUser, findUserById } from "../Models/User/Queries";
 
 export const usersController = new Elysia({ prefix: "/users" })
 	.get("", async () => {
 		return await findAllUser();
 	})
-	.get("/:id", async ({ params: { id } }) => {
-		return await findUserById(id);
-	});
+	.get(
+		"/:id",
+		async ({ params: { id } }) => {
+			return await findUserById(id);
+		},
+		{
+			params: t.Object({
+				id: t.String(),
+			}),
+		},
+	);
